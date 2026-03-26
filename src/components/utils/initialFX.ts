dc809c54ba0ae6a6bfe1ba9b67d680a86a8cc2f7
atharvshingade-code <atharvshingade7@gmail.com>
2026-03-26 11:49:20 +0530

    final

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,7 +1,7 @@
-import { SplitText } from "gsap-trial/SplitText";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import SplitText from "gsap/SplitText";
 import { lenis } from "../Navbar";
-
+gsap.registerPlugin(SplitText);
 export function initialFX() {
   document.body.style.overflowY = "auto";
   lenis?.start();
